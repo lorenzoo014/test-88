6203b58c094542507546a9a09f5236c2a94da221
lorenzoo22 <lagudmarr@gmail.com>
2021-10-07 00:36:50 +0200

    repositorio1

--- prueba_01.py
+++ prueba_01.py
@@ -1,3 +1,7 @@
-print("Hola mundo")
+
+# Write your python code here
 print("Adios mundo")
 print("Suuuuuuu")
+print("super suuuu")
+print("Hola mundo")
+print("Roses are #ff0000 Violets are #0000ff why my code´s working I haven´t a clue")
